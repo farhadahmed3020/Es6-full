f17e3d78623c7f159680096e85205b680c0a8c12
farhadahmed3020 <farhadahmed3020@gmail.com>
2021-01-10 10:04:44 +0600

    add new

---
+++ letconst.js
@@ -0,0 +1,25 @@
+const name = "farhad ahmed";
+//name = "farhan ahmed" ;
+console.log(name);
+
+const nums = [12,14,16];
+nums [2] = 20 ;
+nums.push(18);
+console.log(nums);
+
+// let variable //
+let studentName = "Farhad";
+studentName = "Eva";
+console.log(studentName);
+
+// var vs let //
+let sum = 0;
+// for(var i = 0 ; i< 10 ; i++){
+//     sum = sum + 1;
+// }
+// console.log(i);
+
+for(let i = 0; i<10 ; i++){
+    sum = sum +1 ;
+}
+console.log(i);
