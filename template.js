3053189810b804a91022f88383051af087f0bbef
farhadahmed3020 <farhadahmed3020@gmail.com>
2021-01-13 16:34:48 +0600

    addad

--- template.js
+++ template.js
@@ -1,4 +1,9 @@
 const firstName = "Justin" ;
 const lastName = " TimberLake " ;
 const fullName = firstName + " " + lastName + " is a good person " ;
-console.log(fullName);
+const fullName2 = `${firstName} ${54+88} is a good boy.`;
+const multiLine = " I Love you\n" +" I miss You\n" +"I need you";
+const multiLine2 = ` I love you 
+ I miss  you baby`;
+console.log(fullName2);
+console.log(multiLine2);
